634adb52e00d82d6611e60ba80621d2f3db4b894
Coder-Iro <Coder-Iro@teamif.io>
2021-03-23 17:11:28 +0900

    Make blank route page

---
+++ pages/member.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Member',
+}
+</script>
+
+<style scoped></style>
---
+++ pages/notice.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Notice',
+}
+</script>
+
+<style scoped></style>
