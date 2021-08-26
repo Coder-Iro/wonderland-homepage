89e0b1a3b78b5f34fd05a74647f7ce5d02367fa0
이로 <Coder-Iro@teamif.io>
2021-08-26 09:13:56 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,10 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 2, // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start',
+      env: {
+          "API_URL_BROWSER": "https://wonder.teamif.io/",
+      }
     }
   ]
 }
