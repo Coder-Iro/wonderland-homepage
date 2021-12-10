eee3b977f92d90201ddbbc1c72a516ba521837ad
이로 <coder-iro@teamif.io>
2021-12-10 13:58:27 +0900

    fix(contents): :construction: fix css
    Add css for img tag

--- components/ContentBox.vue
+++ components/ContentBox.vue
@@ -1,10 +1,21 @@
 <template>
   <box>
-    <h3 :style="actived ? ['border-width: 1px;'] : ['']">
-      <a>
+    <h3
+      :style="
+        actived
+          ? [
+              'border-width: 1px;',
+              'padding-bottom: 10px;',
+              'margin-bottom: 10px;',
+            ]
+          : undefined
+      "
+    >
+      <button type="button" @click="actived = !actived">
         <strong :class="content.color">{{ content.title }}</strong>
-      </a>
+      </button>
     </h3>
+
     <section v-show="actived">
       <img v-if="content.img" :src="`assets/img/contents/${content.id}.png`" />
     </section>
@@ -28,15 +39,28 @@
   });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   h3 {
+    height: 100%;
     font-size: 19px;
     letter-spacing: 2px;
     border-bottom: 0px dashed #e3e3e3;
-    padding-bottom: 10px;
-    margin-bottom: 10px;
-    a {
-      display: block;
+
+    button {
+      width: 100%;
+      background: none;
+      font-size: inherit;
+      border: none;
+      letter-spacing: 2px;
+      text-align: left;
+      padding: 0;
+    }
+  }
+  section {
+    img {
+      margin-bottom: 15px;
+      border-radius: 3px;
+      box-shadow: 2px 2px 3px #555;
     }
   }
 </style>
